8635768b829389c951076f94683e00a823761639
SimonArias77 <sariass077@gmail.com>
2024-05-15 05:34:40 -0500

    Plantilla Crud-Vite

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// Import our custom CSS
+import '../scss/style.scss'
+import { variableAexportar} from "../../public/data/dataBase.js"; //Esto es solo un ejemplo por si tenemos ese
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
